85e4517a97546c351ab41907187ca3b487f2066a
kandarpagalas <kandarpa.galas@gmail.com>
2023-06-27 11:28:31 -0300

    testing os.environ

--- app01/hello.py
+++ app01/hello.py
@@ -1,8 +1,14 @@
 import fire
+import os
+
 
 def hello(name="World"):
-  print(f"Hello {name}!")
-  return "Hello %s!" % name
+    params = os.environ
+    for param in params:
+        print(f"param: {param}")
+
+    return "Hello %s!" % name
+
 
 if __name__ == '__main__':
-  fire.Fire(hello)
+    fire.Fire(hello)
